272c6a92b366b241dc1e8d6cc62a635984f3ee6f
AndreasD83 <dieckmann.andreas@gmail.com>
2021-09-24 15:24:36 +0200

    add copy sh

---
+++ scp.sh
@@ -0,0 +1,10 @@
+scp rules/* pi@openhabiandevice:/etc/openhab/rules/
+scp items/* pi@openhabiandevice:/etc/openhab/items/
+scp html/* pi@openhabiandevice:/etc/openhab/html/
+scp icons/* pi@openhabiandevice:/etc/openhab/icons/
+scp persistence/* pi@openhabiandevice:/etc/openhab/persistence/
+scp services/* pi@openhabiandevice:/etc/openhab/services/
+scp sitemaps/* pi@openhabiandevice:/etc/openhab/sitemaps/
+scp sounds/* pi@openhabiandevice:/etc/openhab/sounds/
+scp things/* pi@openhabiandevice:/etc/openhab/things/
+
