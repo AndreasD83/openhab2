17f88df740eb0d4796213f9a715e02ef677adee6
dieckmann.andreas@gmail.com <dieckmann.andreas@gmail.com>
2024-12-29 13:38:22 +0100

    change labels, hostname

--- scp.sh
+++ scp.sh
@@ -1,10 +1,10 @@
-scp rules/* pi@openhabiandevice:/etc/openhab/rules/
-scp items/* pi@openhabiandevice:/etc/openhab/items/
-scp html/* pi@openhabiandevice:/etc/openhab/html/
-scp icons/* pi@openhabiandevice:/etc/openhab/icons/
-scp persistence/* pi@openhabiandevice:/etc/openhab/persistence/
-scp services/* pi@openhabiandevice:/etc/openhab/services/
-scp sitemaps/* pi@openhabiandevice:/etc/openhab/sitemaps/
-scp sounds/* pi@openhabiandevice:/etc/openhab/sounds/
-scp things/* pi@openhabiandevice:/etc/openhab/things/
+scp rules/* openhabian@homebase:/etc/openhab/rules/
+scp items/* openhabian@homebase:/etc/openhab/items/
+scp html/* openhabian@homebase:/etc/openhab/html/
+scp icons/* openhabian@homebase:/etc/openhab/icons/
+scp persistence/* openhabian@homebase:/etc/openhab/persistence/
+scp services/* openhabian@homebase:/etc/openhab/services/
+scp sitemaps/* openhabian@homebase:/etc/openhab/sitemaps/
+scp sounds/* openhabian@homebase:/etc/openhab/sounds/
+scp things/* openhabian@homebase:/etc/openhab/things/
 
